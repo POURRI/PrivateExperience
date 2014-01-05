274ed065aaf4c118c4b369bc8dec2ff6ade8b0b9
POURRI <pourri@mail.ru>
2014-01-05 17:17:31 +0200

    add file
    with test data

--- projects/Java/EnglishRusBook/src/englishrusbook/EnglishRusBook.java
+++ projects/Java/EnglishRusBook/src/englishrusbook/EnglishRusBook.java
@@ -6,7 +6,7 @@
 
 package englishrusbook;
 
-import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfReader;
 /**
  *
  * @author POURRI
@@ -16,12 +16,26 @@ public class EnglishRusBook {
     /**
      * @param args the command line arguments
      */
+    
+    public static final String PDF = "test/data/mini.pdf";
+    
     public static void main(String[] args) {
         parsePDF();
     }
     
-    public static void parsePDF() {
-        System.out.println("parsePDF");
+    private static void parsePDF() {
+        try {
+            PdfReader reader = new PdfReader(PDF);
+            
+            debugOut("parsePDF");
+        } catch (Exception e) {
+            debugOut("We have error when create PdfReader");
+            debugOut("if you see this - you must do something ok?");
+        }
+    }
+    
+    private static void debugOut(String debug) {
+        System.out.println(debug);
     }
     
 }
