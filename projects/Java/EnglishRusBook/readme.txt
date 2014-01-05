baf21972d56d26beaa25864db78384c1c9c30524
POURRI <pourri@mail.ru>
2014-01-06 01:11:24 +0200

    do
    read and write PDF

--- projects/Java/EnglishRusBook/readme.txt
+++ projects/Java/EnglishRusBook/readme.txt
@@ -1,1 +1,3 @@
-﻿iText videoInfo http://youtu.be/5RZmySwMeYM
+﻿	List:
+1. iText videoInfo http://youtu.be/5RZmySwMeYM
+2. http://www.vogella.com/tutorials/JavaPDF/article.html
--- projects/Java/EnglishRusBook/src/englishrusbook/EnglishRusBook.java
+++ projects/Java/EnglishRusBook/src/englishrusbook/EnglishRusBook.java
@@ -1,60 +1,101 @@
 package englishrusbook;
 
+import java.io.FileOutputStream;
+
+import com.itextpdf.text.Document;
+
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 
+import java.io.IOException;
+import com.itextpdf.text.DocumentException;
+
+import java.io.FileNotFoundException;
+
 public class EnglishRusBook {
 
-    private static String PDF;
+    private static String PDFToRead = "test/data/TheTestRead.pdf";
+    private static String PDFToWrite = "test/data/TheTestWrite.pdf";
     
-    private static boolean isDebug;
+    private static boolean isDebug = false;
+    
+    private static PdfReader reader;
     
     public static void main(String[] args) {
-        initSettings();
-        parsePDF();
-    }
-    
-    private static void initSettings() {
-        PDF = "test/data/mini.pdf";
-        isDebug = true;
-    }
-    
-    private static void parsePDF() {
-        debugOut("function:parsePDF");
-        try {
-            PdfReader reader = new PdfReader(PDF);
+        if (isCanParsingPDF()) {
             showPDFInfo(reader);
-            showPDFPages(reader);  
-        } catch (Exception e) {
-            debugOut("We have error when create PdfReader");
-            System.out.println(e);
-            debugOut("if you see this - you must do something ok?");
+            String lineList[] = getPDFPages(reader);
+            createPDF(lineList);
         }
     }
     
-    private static void showPDFInfo(PdfReader reader) {
-        debugOut("function:showPDFInfo");
-        printOut("FileLength: " + String.valueOf(reader.getFileLength()));
-        printOut("NumberOfPages: " + String.valueOf(reader.getNumberOfPages()));
+    private static void createPDF(String[] lineList) {
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream(PDFToWrite));
+            writeToPDF(document, lineList);
+        } catch (FileNotFoundException | DocumentException e) {
+            System.out.println("ERROR: " + e); 
+        }
     }
     
-    private static void showPDFPages(PdfReader reader) {
-        debugOut("function:showPDFPages");
-        
-        for (int i = 0, count = reader.getNumberOfPages(); i < count; i++) {
-            printOut("ShowPageOfNumber: " + String.valueOf(i));
-            try {
-                printOut("ShowPage: " + PdfTextExtractor.getTextFromPage(reader, i));
-            } catch (Exception e) {
-                debugOut("Can't show Page: " + String.valueOf(i));
-                System.out.println(e);
+    private static void writeToPDF(Document document, String[] lineList) {
+        document.open();
+        try {
+            for (int i = 1, lineListCount = lineList.length; i < lineListCount; i++) {
+                document.add(new Paragraph(lineList[i]));
             }
-        }    
+        } catch (DocumentException e) {
+            System.out.println("ERROR in writing: " + e);
+        } finally {
+            document.close();
+        }
+    }
+    
+    private static boolean isCanParsingPDF() {
+        debugOut("function:isCanParsingPDF");
+        boolean isCanParsingPDF = true;
+        
+        try {
+            reader = new PdfReader(PDFToRead);
+        } catch (IOException e) {
+            isCanParsingPDF = false;
+        }
+        
+        return isCanParsingPDF;
+    }
+    
+    private static void showPDFInfo(PdfReader reader) {
+        printOut("INFO: FileLength: " + String.valueOf(reader.getFileLength()));
+        printOut("INFO: NumberOfPages: " + String.valueOf(reader.getNumberOfPages()));
+    }
+    
+    private static String[] getPDFPages(PdfReader reader) {
+        debugOut("function:getPDFPages");
+        
+        int pageCount = reader.getNumberOfPages();
+        
+        // we have different start in Array and Page
+        String lineList[] = new String[pageCount + 1];
+        
+        for (int i = 1; i <= pageCount; i++) {
+            debugOut("ShowPageOfNumber: " + String.valueOf(i));
+            try {
+                lineList[i] = PdfTextExtractor.getTextFromPage(reader, i);
+            } catch (IOException e) {
+                System.out.println("ERROR in reading: " + e);
+            }
+        }
+        
+        return lineList;
     }
     
     private static void debugOut(String debug) {
         if (!isDebug) return; 
-        printOut(debug);
+        printOut("DEBUG: " + debug);
     }
     
     private static void printOut(String print) {
