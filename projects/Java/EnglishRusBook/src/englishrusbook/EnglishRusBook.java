5f505c9d8c05453da463304db73c5013711f16d2
POURRI <pourri@mail.ru>
2014-01-09 01:46:00 +0200

    change to better

--- projects/Java/EnglishRusBook/src/englishrusbook/EnglishRusBook.java
+++ projects/Java/EnglishRusBook/src/englishrusbook/EnglishRusBook.java
@@ -15,13 +15,14 @@ import com.itextpdf.text.DocumentException;
 
 import java.io.FileNotFoundException;
 import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class EnglishRusBook {
 
-    private static String PDFToRead = "test/data/TheTestRead.pdf";
-    private static String PDFToWrite = "test/data/TheTestWrite.pdf";
+    private final static String PDFToRead = "test/data/TheTestRead.pdf";
+    private final static String PDFToWrite = "test/data/TheTestWrite.pdf";
     
-    private static boolean isDebug = false;
+    private final static boolean isDebug = true;
     
     private static PdfReader reader;
     
@@ -30,8 +31,7 @@ public class EnglishRusBook {
         
         showPDFInfo(reader);
         String lineList[] = getPDFPages(reader);
-        getUnique(lineList);
-        createPDF(lineList);
+        createPDF(getUnique(lineList));
     }
     
     private static void createPDF(String[] lineList) {
@@ -44,18 +44,29 @@ public class EnglishRusBook {
         }
     }
     
-    private static HashSet <String> getUnique(String[] lineList) {
-        HashSet <String> full = new HashSet <String>();
-
-        for (int i = 1; i < lineList.length; i++) {
-            
-            String[] words = lineList[i].split(" ");
-            
-            for (String word: words){
-                full.add(word);
+    private static String[] getUnique(String[] lineList) {
+        HashSet<String> full = new HashSet<String>();
+        
+        int debugAllCount = 0;
+        
+        String discargChars = " 1234567890~(){}[]%<>+=$#*!?.,:;-\'\"/_`";
+        
+        int minWordLength = 1;
+        for (String line: lineList) {
+            StringTokenizer token = new StringTokenizer(line, discargChars);
+            while (token.hasMoreElements()) {
+                debugAllCount++;
+                String word = token.nextToken();
+                if (word.length() > minWordLength) {
+                    full.add(word.toLowerCase());
+                }
             }
         }
-        return full;
+        
+        debugOut("AllCount = " + String.valueOf(debugAllCount));
+        debugOut("UniqueCount = " + String.valueOf(full.size()));
+        
+        return full.toArray(new String[full.size()]);
     }
     
     private static void writeToPDF(Document document, String[] lineList) {
@@ -95,12 +106,12 @@ public class EnglishRusBook {
         int pageCount = reader.getNumberOfPages();
         
         // we have different start in Array and Page
-        String lineList[] = new String[pageCount + 1];
+        String lineList[] = new String[pageCount];
         
         for (int i = 1; i <= pageCount; i++) {
             debugOut("ShowPageOfNumber: " + String.valueOf(i));
             try {
-                lineList[i] = PdfTextExtractor.getTextFromPage(reader, i);
+                lineList[i - 1] = PdfTextExtractor.getTextFromPage(reader, i);
             } catch (IOException e) {
                 System.out.println("ERROR in reading: " + e);
             }
